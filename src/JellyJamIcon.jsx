fa60fcd8688a07ec6d546fd6da932306d05d174b
JamesJDev <jamejohn2005@gmail.com>
2025-09-23 12:11:39 -0400

    Adjusted Icons

--- src/JellyJamIcon.jsx
+++ src/JellyJamIcon.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import jellyJamLogo from './assets/jellyjam.png';
+import youtubeIcon from './assets/youtubeLogo.png';
+import itchIOLogo from './assets/itchio.png';
 
 const JellyJamIcon = () => {
     return (
         <div style={{ textAlign: 'center' }}>
-            <div>
-                <h1>JellyJam</h1>
+            <h1>JellyJam Channel</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '24px', marginBottom: '12px' }}>
                 <a href="https://www.youtube.com/c/@JellyJamDev" target="_blank" rel="noopener noreferrer">
-                    <img src={jellyJamLogo} className="jellyjam" alt="JellyJam Logo" style={{ width: '100px', height: '100px' }}/>
+                    <img src={youtubeIcon} className="jellyjam" alt="YouTube Logo" style={{ width: '145px', height: '100px' }} />
+                </a>
+            </div>
+            <h1>Try out my Games!</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', gap: '24px', marginBottom: '12px' }}>
+                <a href="https://jellyjamdev.itch.io" target="_blank" rel="noopener noreferrer">
+                    <img src={itchIOLogo} className="jellyjam" alt="Itch.io Logo" style={{ width: '100px', height: '100px' }} />
                 </a>
             </div>
         </div>
